4ea38ef3cfbf9607ef3dd97fc073d1e7c6344abe
Daniel Popescu <same.dan@gmail.com>
2023-04-24 17:40:00 +0200

    Started Middleware

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@ use App\Http\Controllers\UserController;
 */
 
 // USER routes
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
+Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login'); // rename the page "name"
 
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
 
 // BLOG Routes
-Route::get("/create-post", [PostController::class, "showCreateForm"]);
+Route::get("/create-post", [PostController::class, "showCreateForm"])->middleware('auth');
 Route::post("/create-post", [PostController::class, "storeNewPost"]);
 
 Route::get("/post/{post}", [PostController::class, "viewSinglePost"]);
