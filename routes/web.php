b40522bc12bf56dff731cfc78a3485992b8b73ca
Daniel Popescu <same.dan@gmail.com>
2023-04-25 17:28:25 +0200

    Gate in routes middleware

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,8 @@ use App\Http\Controllers\UserController;
 
 // ADMIN routes
 Route::get('/admins-only', function() {
-   if(Gate::allows('visitAdminPages')) {
     return "only visible to admins";
-   }
-   return "Not available";
-});
+})->middleware('can:visitAdminPages');
 
 // USER routes
 Route::get('/', [UserController::class, "showCorrectHomepage"])->name('login'); // rename the page "name"
