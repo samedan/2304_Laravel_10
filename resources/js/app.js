ce8b1b6cb081a28557ada067ba08cd82ff29f805
Daniel Popescu <same.dan@gmail.com>
2023-04-28 12:26:50 +0200

    Search on FrontEnd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import "./bootstrap";
+import Search from "./live-search";
+
+// load search onlyu if yoy are logged in
+if (document.querySelector(".header-search-icon")) {
+    new Search();
+}
